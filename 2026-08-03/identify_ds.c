5015ef328ae74e85cb7a7e768ba1d8a152a76ed4
unknown <subhrakantibera01@gmail.com>
2026-08-16 20:30:00 +0530

    1st Day

--- 2026-08-03/identify_ds.c
+++ 2026-08-03/identify_ds.c
@@ -25,24 +25,16 @@ int main() {
         else if (strcmp(input, "linked list") == 0 || strcmp(input, "linkedlist") == 0) {
             printf("%s is a Non-Primitive, Linear, Dynamic data structure.\n", original_input);
         } 
-        else if (strcmp(input, "stack") == 0 || strcmp(input, "queue") == 0) {
-            printf("%s is a Non-Primitive, Linear abstract data structure (can be static or dynamic).\n", original_input);
-        } 
         else if (strcmp(input, "tree") == 0 || strcmp(input, "graph") == 0) {
             printf("%s is a Non-Primitive, Non-Linear data structure.\n", original_input);
         } 
-        else if (strcmp(input, "hash table") == 0 || strcmp(input, "hash map") == 0) {
-            printf("%s is a Non-Primitive, Non-Linear data structure.\n", original_input);
-        } 
         else if (strcmp(input, "integer") == 0 || strcmp(input, "int") == 0 || 
-                 strcmp(input, "float") == 0 || strcmp(input, "char") == 0 || 
-                 strcmp(input, "boolean") == 0 || strcmp(input, "bool") == 0) {
+                 strcmp(input, "float") == 0 || strcmp(input, "char") == 0) {
             printf("%s is a Primitive data structure.\n", original_input);
         } 
         else {
-            printf("Classification for '%s' is not found. Please try standard names like Array, Tree, or Stack.\n", original_input);
+            printf("Classification for '%s' is not found. Please try standard names like Array, Tree, or Linked List.\n", original_input);
         }
     }
-
     return 0;
 }
